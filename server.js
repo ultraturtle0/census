f306561d6b2ef895e0b8a9c07ce56f5a36a6c0e3
jkusel <jordankusel@protonmail.com>
2020-01-24 01:08:20 -0800

    Revert "bug squashing"
    This reverts commit bf4b7563dc1942bef6466a5e397b09cca2f25ace.

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ app.route('/api/status')
     .get((req, res, next) => res.send({ message: 'active' }))
     .post((req, res, next) => {
         var code = 0;
-        if (req.body.command === 'poweroff') {
-            code = shell.exec('poweroff', function (code, stdout, stderr) {});
-            return res.send({ message: 'success!' });
-        } else {
+        if (req.body.command === 'poweroff')            
+		code = shell.exec('poweroff').code;
+        
+        if (!code)
             return res.status(500).send({ errors: ['command failed.'] });
-        };
+        else
+            return res.send({ message: 'success!' });
     });
 
 
